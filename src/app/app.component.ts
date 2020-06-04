56f9f2f26db70e548b16883b088196b9273720da
mtkramer <kramerm1@mail.usf.edu>
2020-06-04 18:25:20 -0400

    completed assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,11 @@ export class AppComponent {
   log: string;
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
 
   constructor() {
-    this.sourceList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
-
+    this.sourceList = [];
     window.fetch(satellitesUrl).then(function(response) {
       response.json().then(function(data) {
 
@@ -28,9 +28,28 @@ export class AppComponent {
             sat.operational
           ));
         }
+        this.displayList = this.sourceList;
 
       }.bind(this));
     }.bind(this));
+  }
+
+  search(searchTerm: string): void {
+    searchTerm = searchTerm.toLowerCase();
+    if (searchTerm) {
+      this.displayList = [];
+      for (let sat of this.sourceList) {
+        sat.name.toLowerCase() === searchTerm ? this.displayList.push(sat) : null;
+      }
+    }
+    else {
+      this.displayList = this.sourceList;
+      window.alert("No search terms entered");
+    }
+    if (this.displayList.length === 0) {
+      this.displayList = this.sourceList;
+      window.alert("No matches to that search");
+    }
   }
 
 }
